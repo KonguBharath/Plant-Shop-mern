5c010c3efe34fc10d212c19e512e81d2e8915b42
KonguBharath <bharathviswa06@gmail.com>
2024-09-29 13:57:34 +0530

    we have implemented the react-Query component to fetch all data frm each component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,10 +13,12 @@ import './index.css';
 import Homepage from './pages/HomePage.tsx';
 
 import ProductPage from './pages/ProductPage.tsx';
-import axios from 'axios';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+/*import axios from 'axios';*/
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/';
+/*axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/';*/
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,10 +30,15 @@ const router = createBrowserRouter(
     </Route>
   )
 );
+
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HelmetProvider>
   </StrictMode>
 );
--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,6 +1,11 @@
+import { Helmet } from 'react-helmet-async';
+
 export default function ProductPage() {
   return (
     <div>
+      <Helmet>
+        <title>Product-NB</title>
+      </Helmet>
       <h2>product page</h2>
     </div>
   );
