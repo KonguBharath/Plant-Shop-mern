421d99139118b03bed145cc9c798aa8ef37092bd
KonguBharath <bharathviswa06@gmail.com>
2024-09-29 07:32:16 +0530

    adding product data to the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css';
+import { sampleProducts } from './data';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <head>Nature's Breeze</head>
-      <main></main>
+      <header>Nature's Breeze</header>
+      <main>
+        <ul>
+          {sampleProducts.map((Product) => (
+            <li key={Product.slug}>
+              <img
+                src={Product.image}
+                alt={Product.name}
+                className="product-image"
+              />
+              <h2>{Product.name}</h2>
+              <p>{Product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved to Nature's Breeze</footer>
     </div>
   );
---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  name: string;
+  slug: string;
+  image: string;
+  categories: string;
+  brand: string;
+  price: number;
+  countInStock: number;
+  description: string;
+  rating: number;
+  numReviews: number;
+};
